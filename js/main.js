1e50b4e3467f92991e3eff922f6abed6c64f4de9
Breena Troy <breena17@gmail.com>
2018-09-15 16:29:18 -0700

    responsive design complete

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYnQxNyIsImEiOiJjam0yeDB4ZW8wOGM2M3duMnJmamE5Z2M0In0.LvuMSk8AjLt_qGsJbkfw4w',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
